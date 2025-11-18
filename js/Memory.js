fbcce8f0a954475ceb635782fa63b1bae05bae4b
Clara Huet <clara.huet2025@campus-eni.fr>
2025-11-18 19:50:21 +0100

    cerveau compote

--- js/Memory.js
+++ js/Memory.js
@@ -4,60 +4,39 @@ const cartes = document.getElementsByClassName('cartes');
 
 
 // fonction génerale qui appel toutes les fonctions
-export function initialisation(){
+export function initialisation() {
     // initialisation des cartes avec espaces
-document.addEventListener("keyup", (event) =>{
-    if (event.key == " "){
-        cartesInit();
-    }
-});
-// initialisation du click sur les cartes
-// document.getElementsByClassName("cartes").addEventListener("click",retourne);
+    document.addEventListener("keyup", (event) => {
+        if (event.key == " ") {
+            cartesInit();
+        }
+    });
+    // initialisation du click sur les cartes
+    // document.getElementsByClassName("cartes").addEventListener("click",retourne);
 
 
 }
 // fonction d'initialisation pour afficher les cartes retourner
 
- function cartesInit(){
-for (const carte in cartes){
+function cartesInit() {
+let i = 1;
+    for (const carte of cartes) {
+        
+        carte.textContent = i++;
 
-    console.log("cartes initialiser")
-}
+        console.log("cartes initialiser")
+    }
 }
 
 // fonction qui permet de cliquer sur les cartes et qu'elle se retourne
 
-$(function (){
-    // variables pour le nombre d'essai
-let nbEssai = 0;
 
-    // Je commence simple sans choix de taille ni de thème...
-    // A complexifier plus tard
-    let match1 = '<img src="ressources/memory-legume/1.svg">';
-    // let match2 = '<img src="ressources/memory-legume/2.svg">';
-    // let match3 = '<img src="ressources/memory-legume/3.svg">';
-    // let match4 = '<img src="ressources/memory-legume/4.svg">';
-    // let match5 = '<img src="ressources/memory-legume/5.svg">';
-    // let match6 = '<img src="ressources/memory-legume/6.svg">';
-
-    // Jouer un essai
-        $(".carte").on("click", revelerCarte);
-
-function revelerCarte() {
-    if ($(this).attr("class") === "carte") { // "carte" = face cachée
-        // Reveler la carte
-        $(this).html(match1);
-        console.log("Flip card")
-    }
-}
-
-})
 //  fonction qui permet de verifier si 2 cartes sont identiques ou non dans ce cas elle se retourne au bout de 5 secondes.
 // function verification(){
-    // let f = 2;
-    // for(let i = 0; i<f; i++ ){
-    //     retourne();
-    // }
-    // if ()
+// let f = 2;
+// for(let i = 0; i<f; i++ ){
+//     retourne();
+// }
+// if ()
 // }
 
