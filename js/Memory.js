58c8c6f6664a2581a18bb6eb07b570f0386ce928
Clara Huet <clara.huet2025@campus-eni.fr>
2025-11-18 16:40:25 +0100

    css fait pour les pages, encore les bouton a modifier et les selects

--- js/Memory.js
+++ js/Memory.js
@@ -1,3 +1,8 @@
+// variables
+const cartes = document.getElementsByClassName('cartes');
+
+
+
 // fonction génerale qui appel toutes les fonctions
 export function initialisation(){
     // initialisation des cartes avec espaces
@@ -7,18 +12,52 @@ document.addEventListener("keyup", (event) =>{
     }
 });
 // initialisation du click sur les cartes
-document.addEventListener("click",retourne);
+// document.getElementsByClassName("cartes").addEventListener("click",retourne);
 
 
 }
 // fonction d'initialisation pour afficher les cartes retourner
+
  function cartesInit(){
-    let cartes = document.getElementById('cartes');
-    cartes.src = ".\ressources\question.svg";
-console.log("cartes initialiser")
+for (const carte in cartes){
+
+    console.log("cartes initialiser")
+}
 }
 
 // fonction qui permet de cliquer sur les cartes et qu'elle se retourne
-function retourne(){
 
+$(function (){
+    // variables pour le nombre d'essai
+let nbEssai = 0;
+
+    // Je commence simple sans choix de taille ni de thème...
+    // A complexifier plus tard
+    let match1 = '<img src="ressources/memory-legume/1.svg">';
+    // let match2 = '<img src="ressources/memory-legume/2.svg">';
+    // let match3 = '<img src="ressources/memory-legume/3.svg">';
+    // let match4 = '<img src="ressources/memory-legume/4.svg">';
+    // let match5 = '<img src="ressources/memory-legume/5.svg">';
+    // let match6 = '<img src="ressources/memory-legume/6.svg">';
+
+    // Jouer un essai
+        $(".carte").on("click", revelerCarte);
+
+function revelerCarte() {
+    if ($(this).attr("class") === "carte") { // "carte" = face cachée
+        // Reveler la carte
+        $(this).html(match1);
+        console.log("Flip card")
+    }
 }
+
+})
+//  fonction qui permet de verifier si 2 cartes sont identiques ou non dans ce cas elle se retourne au bout de 5 secondes.
+// function verification(){
+    // let f = 2;
+    // for(let i = 0; i<f; i++ ){
+    //     retourne();
+    // }
+    // if ()
+// }
+
